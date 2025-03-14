ab7c97c7641ea4ca9977e5c4826812697b5e671c
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-03-14 15:49:17 +0530

    python chapter 2 completed

---
+++ chapter 2/problem4.py
@@ -0,0 +1,7 @@
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+
+c=(a+b)/2;
+
+print("your both numbers are : " ,a,"and " ,b)
+print("Avg of these numbers is : ",c);
---
+++ chapter 2/problem5.py
@@ -0,0 +1,3 @@
+a=int(input("Enter the Number : "))
+
+print("The square of the number is : ",a*a)
